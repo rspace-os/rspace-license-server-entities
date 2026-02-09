24d3117f486493cb247a4c53012c6ccb1b8152a4
Fraser <141634676+rs-fraser@users.noreply.github.com>
2026-02-09 12:24:12 +0000

    migrate to jakarta & Spring 6

--- src/main/java/com/researchspace/licenseserver/model/ServerInfo.java
+++ src/main/java/com/researchspace/licenseserver/model/ServerInfo.java
@@ -1,11 +1,11 @@
 package com.researchspace.licenseserver.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 
 /**
  * Basic info about the customer's server
